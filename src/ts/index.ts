e901326b7d0ccc43c6e397f9cd3941f24f90ecd6
Nugine <Nugine@163.com>
2019-02-19 00:06:20 +0800

    import lit-element

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element'
+
+@customElement('my-element')
+class MyElement extends LitElement {
+    public render() {
+        return html`<span>${new Date()}</span>`
+    }
+}
+
 console.debug('hello, world')
 console.error(new Error('dlrow ,olleh'))
